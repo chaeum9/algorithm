b433961f6e754297b625969866dcdc517e7fc118
Chaewon Lee <87990439+chaeum9@users.noreply.github.com>
2025-12-24 10:19:16 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,32 +1,26 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
     static int dp[] = new int[11];
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
         Arrays.fill(dp, -1);
 
-        dp[1] = 1; // 1
-        dp[2] = 2; // 2, 1+1
-        dp[3] = 4; // 3, 1+2, 2+1, 1+1+1
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
 
-        for(int i = 1; i <= T; i++) {
+        for(int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            funcSum(n);
-            sb.append(dp[n]);
-            if(i < T)
-                sb.append("\n");
+            System.out.println(topDown(n));
         }
-        System.out.print(sb);
     }
 
-    public static int funcSum (int n) {
-        if(dp[n] != -1)
-            return dp[n];
-        return dp[n] = funcSum(n-3) + funcSum(n-2) + funcSum(n-1);
+    static int topDown(int n) {
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = topDown(n-1) + topDown(n-2) + topDown(n-3);
     }
 }
