815adc4917aa10fd4d1cf3e088ae747110b99bbd
Chaewon Lee <87990439+chaeum9@users.noreply.github.com>
2025-12-26 14:23:41 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long dp[][] = new long[n+1][10];
+        long result = 0;
+        long mod = 10007;
+
+        for(int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j < 10; j++) {
+                if(j == 0) {
+                    dp[i][0] = 1;
+                } else {
+                    dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod;
+                }
+            }
+        }
+
+        for(int i = 0; i < 10; i++) {
+            result += dp[n][i] % mod;
+        }
+
+        System.out.print(result % mod);
+    }
+}
